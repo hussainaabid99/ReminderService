8605b829dffa86daf13c135fe02296e2114946b8
Aabid <hussain.aabid2002@gmail.com>
2023-08-30 19:00:48 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const cron = require('node-cron');
+const jobs = require('./utils/job');
 
 // const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
 
 const setupAndStartServer = () => {
           const app = express();
           app.use(bodyParser.json());
           app.use(bodyParser.urlencoded({ extended: true }));
 
+          app.post('/api/v1/tickets', TicketController.create);
+
           app.listen(PORT, () => {
                     console.log(`Server started at PORT: ${PORT}`);
+                    jobs();
 
                     // sendBasicEmail(
                     //           'support@admin.com',
@@ -23,9 +27,7 @@ const setupAndStartServer = () => {
                     // );
 
 
-                    cron.schedule('*/2 * * * *', () => {
-                              console.log('running a task every two minutes');
-                    });
+
           });
 }
 
