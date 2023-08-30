fcce29aaecd9266729b4a477fa0db782bdbe8729
Aabid <hussain.aabid2002@gmail.com>
2023-08-30 14:32:01 +0530

    Setup of SMTP using nodemailer done

---
+++ src/config/emailConfig.js
@@ -0,0 +1,15 @@
+//SMTP transporter Object
+const nodemailer = require('nodemailer');
+
+const { EMAIL_ID, EMAIL_PASS } = require('./serverConfig');
+
+const sender = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+                    user: EMAIL_ID,
+                    pass: EMAIL_PASS
+
+          }
+});
+
+module.exports = sender;
