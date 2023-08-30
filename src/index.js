c6e15f433fb391abb0de44c5a9a7ec6ded422aea
Aabid <hussain.aabid2002@gmail.com>
2023-08-30 15:47:23 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+const cron = require('node-cron');
+
+// const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = () => {
           const app = express();
@@ -13,12 +15,17 @@ const setupAndStartServer = () => {
           app.listen(PORT, () => {
                     console.log(`Server started at PORT: ${PORT}`);
 
-                    sendBasicEmail(
-                              'support@admin.com',
-                              'notificationservice20@gmail.com',
-                              'This is a testing email',
-                              'Hey, how are you, I hope you like the support'
-                    );
+                    // sendBasicEmail(
+                    //           'support@admin.com',
+                    //           'notificationservice20@gmail.com',
+                    //           'This is a testing email',
+                    //           'Hey, how are you, I hope you like the support'
+                    // );
+
+
+                    cron.schedule('*/2 * * * *', () => {
+                              console.log('running a task every two minutes');
+                    });
           });
 }
 
